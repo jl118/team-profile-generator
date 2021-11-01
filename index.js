3481ed460fde7aab94633f71ff28713bae45b580
jl118 <jen.liebelt@gmail.com>
2021-11-01 17:52:00 -0400

    Added initial TODO's in javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+// external packages and dependencies
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// team profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// page creator
+const generateHTML = require('./src/generateHTML');
+
+// TODO: write questions array
+
+// TODO: create fs function 
+
