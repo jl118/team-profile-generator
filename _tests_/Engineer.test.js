9b57db60d39f0fdecafb204de8a1771476a19c3a
jl118 <jen.liebelt@gmail.com>
2021-11-20 17:03:01 -0500

    updated engineer test

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,21 +2,21 @@ const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
     it("Should create an object with name, id, email, and github", ()=> {
-        expect(new Engineer("Sully Sullenberger", 193, "sullen.berger@gmail.com", "sberger")).toEqual({ name: 'Sully Sullenberger', id: 193, email: 'sullen.berger@gmail.com', github: 'sberger' });
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully")).toEqual({ name: 'James P. Sullivan', id: 193, email: 'sullen.berger@gmail.com', github: 'sberger' });
     });
     it("should be able to call getName and get the engineer's name", ()=> {
-        expect(new Engineer("Sully Sullenberger", 193, "sullen.berger@gmail.com", "sberger").getName()).toEqual("Sully Sullenberger");
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getName()).toEqual("James P. Sullivan");
     });
     it("should be able to call getId and get the engineer's id", ()=> {
-        expect(new Engineer("Sully Sullenberger", 193, "sullen.berger@gmail.com", "sberger").getId()).toBe(193);
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getId()).toBe(193);
     });
     it("should be able to call getEmail and get the engineer's email", ()=> {
-        expect(new Engineer("Sully Sullenberger", 193, "sullen.berger@gmail.com", "sberger").getEmail()).toBe("sullen.berger@gmail.com");
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getEmail()).toBe("sullen.berger@gmail.com");
     });
     it("should be able to call getGithub and get the engineer's github", ()=> {
-        expect(new Engineer("Sully Sullenberger", 193, "sullen.berger@gmail.com", "sberger").getGithub()).toBe("sberger");
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getGithub()).toBe("sully");
     });
     it("should be able to call getRole and get 'Engineer'", ()=> {
-        expect(new Engineer("Sully Sullenberger", 193, "sullen.berger@gmail.com", "sberger").getRole()).toBe("Engineer");
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getRole()).toBe("Engineer");
     });
 });
