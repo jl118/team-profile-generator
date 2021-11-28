8930f0c08b2eb0571ef6743d5e1e5ffafa564c69
jl118 <jen.liebelt@gmail.com>
2021-11-28 12:41:06 -0500

    fixed typos in Engineer and Manager tests

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,7 +2,7 @@ const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
     it("Should create an object with name, id, email, and github", ()=> {
-        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully")).toEqual({ name: 'James P. Sullivan', id: 193, email: 'sullen.berger@gmail.com', github: 'sberger' });
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully")).toEqual({ name: 'James P. Sullivan', id: 193, email: 'sullen.berger@gmail.com', github: 'sully' });
     });
     it("should be able to call getName and get the engineer's name", ()=> {
         expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getName()).toEqual("James P. Sullivan");
@@ -13,8 +13,8 @@ describe("Engineer", () => {
     it("should be able to call getEmail and get the engineer's email", ()=> {
         expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getEmail()).toBe("sullen.berger@gmail.com");
     });
-    it("should be able to call getGithub and get the engineer's github", ()=> {
-        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getGithub()).toBe("sully");
+    it("should be able to call getGitHub and get the engineer's github", ()=> {
+        expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getGitHub()).toBe("sully");
     });
     it("should be able to call getRole and get 'Engineer'", ()=> {
         expect(new Engineer("James P. Sullivan", 193, "sullen.berger@gmail.com", "sully").getRole()).toBe("Engineer");
--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -2,7 +2,7 @@ const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
     it("Should create an object with name, id, email, and office number", () => {
-        expect(new Manager("Randall Boggs", 51, "number1scarer@gmail.com", 440)).toEqual({ name: 'Randall Boggs', id: 51, email: 'number1scarer@gmail.com', office: 440 });
+        expect(new Manager("Randall Boggs", 51, "number1scarer@gmail.com", 440)).toEqual({ name: 'Randall Boggs', id: 51, email: 'number1scarer@gmail.com', officeNumber: 440 });
     });
     it("should be able to call getName and get the manager's name", () => {
         expect(new Manager("Randall Boggs", 51, "number1scarer@gmail.com", 440).getName()).toEqual("Randall Boggs");
@@ -13,8 +13,8 @@ describe("Manager", () => {
     it("should be able to call getEmail and get the manager's email", () => {
         expect(new Manager("Randall Boggs", 51, "number1scarer@gmail.com", 440).getEmail()).toBe("number1scarer@gmail.com");
     });
-    it("should be able to call getOffice and get the manager's office number", () => {
-        expect(new Manager("Randall Boggs", 51, "number1scarer@gmail.com", 440).getOffice()).toBe(440);
+    it("should be able to call getOfficeNumber and get the manager's office number", () => {
+        expect(new Manager("Randall Boggs", 51, "number1scarer@gmail.com", 440).getOfficeNumber()).toBe(440);
     });
     it("should be able to call getRole and get 'Manager'", () => {
         expect(new Manager("Randall Boggs", 51, "number1scarer@gmail.com", 440).getRole()).toBe("Manager");
